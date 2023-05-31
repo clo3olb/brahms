40bc472607c6da38417004c3a40829a178773a32
clo3olb <clo3olb@gmail.com>
2023-06-01 01:30:17 +0900

    fix:rename

--- cmd/db_tables.ts
+++ cmd/db_tables.ts
@@ -2,18 +2,22 @@ function getDBSpreadsheet() {
   return SpreadsheetApp.openById(DB_SHEET_ID);
 }
 
-function getStudentTable(
-  spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet
-) {
-  return new Table<Student>(spreadsheet, "학생", StudentTableSchema);
+function getStudentTable() {
+  return new Table<Student>(getDBSpreadsheet(), "학생", StudentTableSchema);
 }
 
-function getWordTestScoreTable(
-  spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet
-) {
+function getWordTestScoreTable() {
   return new Table<WordTestScore>(
-    spreadsheet,
+    getDBSpreadsheet(),
     "단어시험 점수",
     WordTestScoreTableSchema
+  );
+}
+
+function getAttendanceTable() {
+  return new Table<Attendance>(
+    getDBSpreadsheet(),
+    "출석",
+    DBAttendanceTableSchema
   );
 }
