ace5742739f3154807f22b0453e681781ed21658
clo3olb <clo3olb@gmail.com>
2023-05-31 23:27:13 +0900

    add trigger

---
+++ trigger/db_trigger.ts
@@ -0,0 +1,18 @@
+function onDBOpen() {
+  const ui = SpreadsheetApp.getUi();
+  ui.createMenu("관리자 도구")
+    .addSubMenu(
+      ui
+        .createMenu("학습TA 관리자 패널")
+        .addItem("출석시트 새로고침", "resetTeacherAttendanceSheet")
+    )
+    .addToUi();
+}
+
+function addDBTrigger() {
+  const spreadsheet = getDBSpreadsheet();
+  ScriptApp.newTrigger("onDBOpen")
+    .forSpreadsheet(spreadsheet)
+    .onOpen()
+    .create();
+}
