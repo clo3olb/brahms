55b105656366f52a64a597741ca1c11855b29fe2
clo3olb <clo3olb@gmail.com>
2023-06-14 22:22:49 +0900

    change constant name: MERIT_FORM_ID

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 const DB_SHEET_ID = "1-ykYEKPxwDyGVvUoSjTU3_23n55hqsi9xPAIGbER1jA";
 const MANAGER_SHEET_ID = "1-GwqLoAmFAMtTiCdblyty38_dAm8JwMLhKVWUTiunfo";
-const FORM_ID = "1E8FaU4Ayh2rWbDBcWrdCEsLPyTpxoQnetTMJaBwS6D8";
+const MERIT_FORM_ID = "1E8FaU4Ayh2rWbDBcWrdCEsLPyTpxoQnetTMJaBwS6D8";
