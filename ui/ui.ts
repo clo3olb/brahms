61dd9991383631e52bfd104afd9db0462095c9e7
clo3olb <clo3olb@gmail.com>
2023-06-14 22:24:04 +0900

    add auth to db functions

--- ui/ui.ts
+++ ui/ui.ts
@@ -1,4 +1,13 @@
-function onOpen() {
+const AUTH_EMAIL_LIST = [
+  "hyeonwookim@gvcs-mg.org",
+  "clo3olb@gmail.com",
+  "kyso8575@gmail.com",
+];
+
+function onOpen(event: GoogleAppsScript.Events.SheetsOnOpen) {
+  const email = event.user.getEmail();
+  if (!AUTH_EMAIL_LIST.includes(email)) return;
+
   const ui = SpreadsheetApp.getUi();
   ui.createMenu("관리자 도구")
     .addSubMenu(
@@ -23,7 +32,10 @@ function onOpen() {
       ui
         .createMenu("상벌점")
         .addItem("상벌점 설문지 동기화", "updateMeritPointsForm")
-        .addItem("상벌점 시트 헤더 업데이트", "updateMeritSheetHeaders")
+        .addItem(
+          "상벌점 설문지 응답 전체 업데이트",
+          "updateAllMeritPointsFormResponsesToDB"
+        )
     )
     .addSubMenu(
       ui
@@ -76,7 +88,7 @@ function createUUID() {
 
 function addMeritPointFormOnSubmitTrigger() {
   // Find form and add trigger 'onMeritPointFormSubmit'
-  const form = FormApp.openById(FORM_ID);
+  const form = FormApp.openById(MERIT_FORM_ID);
   ScriptApp.newTrigger("onMeritPointFormSubmit")
     .forForm(form)
     .onFormSubmit()
@@ -86,109 +98,5 @@ function addMeritPointFormOnSubmitTrigger() {
 function onMeritPointFormSubmit(
   event: GoogleAppsScript.Events.FormsOnFormSubmit
 ) {
-  const response = event.response;
-  const dbSpreadSheet = SpreadsheetApp.openById(DB_SHEET_ID);
-  const meritSheet = dbSpreadSheet.getSheetByName("상벌점");
-  const meritTable = new Table(meritSheet, "학생 이름", 1);
-  const itemResponses = response.getItemResponses();
-
-  // Get information from the form
-  const items = {};
-  items["이메일"] = response.getRespondentEmail();
-  items["타임 스탬프"] = response.getTimestamp();
-  items["날짜"] = `${response.getTimestamp().getMonth() + 1}월 ${response
-    .getTimestamp()
-    .getDay()}일`;
-  for (let i = 0; i < itemResponses.length; i++) {
-    items[itemResponses[i].getItem().getTitle()] =
-      itemResponses[i].getResponse();
-  }
-
-  // Update meritTable values
-  meritTable.getIds();
-
-  // update DB sheet
-  const headers = meritTable.values[0];
-  let keys = Object.keys(items);
-  let input = new Array(headers.length);
-  for (let i = 0; i < headers.length; i++) {
-    let headerIndex = headers.findIndex((header) => header == keys[i]);
-
-    input[headerIndex] = items[keys[i]];
-  }
-
-  if (
-    meritTable.sheet.getMaxRows() ==
-    meritTable.headerRowIndex + meritTable.getIds().length
-  ) {
-    meritTable.sheet.insertRowAfter(meritTable.sheet.getMaxRows());
-    meritTable.range = meritTable.sheet.getRange(
-      1,
-      1,
-      meritTable.sheet.getMaxRows(),
-      meritTable.sheet.getMaxColumns()
-    );
-  }
-
-  meritTable.values[meritTable.sheet.getLastRow()] = input;
-
-  meritTable.paint();
-}
-
-function updateMeritSheetHeaders() {
-  const dbSpreadSheet = SpreadsheetApp.openById(DB_SHEET_ID);
-  const meritSheet = dbSpreadSheet.getSheetByName("상벌점");
-  const meritForm = FormApp.openById(FORM_ID);
-  const meritTable = new Table(meritSheet, "학생 이름", 1);
-
-  // update values
-  meritTable.getIds();
-  let headers = meritTable.values[0];
-
-  // delete header row
-  let headerRowRange = meritTable.sheet.getRange(
-    meritTable.headerRowIndex,
-    1,
-    1,
-    meritTable.sheet.getLastColumn()
-  );
-  headerRowRange.clearContent();
-
-  // get header contents
-  let newHeaders = meritForm.getItems().map((item) => {
-    if (
-      item.getType() != FormApp.ItemType.SECTION_HEADER &&
-      item.getType() != FormApp.ItemType.PAGE_BREAK
-    ) {
-      return item.getTitle();
-    }
-  });
-  newHeaders.push("이메일");
-  newHeaders.unshift("타임 스탬프");
-  newHeaders.unshift("날짜");
-
-  newHeaders = newHeaders.filter((item) => item);
-
-  for (let i = 0; i < newHeaders.length; i++) {
-    if (!headers.find((header) => newHeaders[i] == header)) {
-      headers.push(newHeaders[i]);
-    }
-  }
-
-  headers = headers.filter((item) => item);
-
-  if (meritTable.sheet.getMaxColumns() < headers.length) {
-    meritTable.sheet.insertColumnAfter(meritTable.sheet.getMaxColumns());
-    meritTable.range = meritTable.sheet.getRange(
-      1,
-      1,
-      meritTable.sheet.getMaxRows(),
-      meritTable.sheet.getMaxColumns()
-    );
-  }
-
-  // update header row
-  meritTable.sheet
-    .getRange(meritTable.headerRowIndex, 1, 1, headers.length)
-    .setValues([headers]);
+  updateMeritPontFormResponse(event.response);
 }
